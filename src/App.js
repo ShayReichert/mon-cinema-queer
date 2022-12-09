5dbb7b4124aed968ac26f8984ab64a70fb79ff95
Shay Reichert <shay.reichert@outlook.fr>
2022-12-09 17:30:54 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import dune from "./assets/image-dune.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <section className="title-wrapper">
+        <h1>
+          MON <span className="red">C</span>
+          <span className="orange">I</span>
+          <span className="yellow">N</span>
+          <span className="green">É</span>
+          <span className="blue">M</span>
+          <span className="purple">A</span> QUEER
+        </h1>
+        <p className="baseline">Les derniers films queer sortis</p>
+        <p className="second-baseline">Et c'est tout.</p>
+      </section>
+
+      <section className="cards-wrapper">
+        <div className="card">
+          <div className="image-wrapper">
+            <div className="image">
+              <img src={dune} alt="Affiche de Dune" />
+            </div>
+            <div className="image-overlay">
+              <div className="content">
+                <div className="info synopsis">
+                  <strong>Synopsis</strong> : L'histoire de Paul Atreides, jeune homme aussi doué que brillant, voué à connaître un destin hors du
+                  commun qui le dépasse totalement. Car, s'il veut préserver l'avenir de sa famille et de son peuple, il devra se rendre sur Dune, la
+                  planète la plus dangereuse de l'Univers. Mais aussi la seule à même de fournir la ressource la plus précieuse capable de décupler la
+                  puissance de l'Humanité. Tandis que des forces maléfiques se disputent le contrôle de cette planète, seuls ceux qui parviennent à
+                  dominer leur peur pourront survivre…
+                </div>
+
+                <div className="info director">
+                  <strong>Réalisateur·ice</strong> : Denis Villeneuve
+                </div>
+
+                <div className="info language">
+                  <strong>Langue originale</strong> : Anglais
+                </div>
+
+                <div className="info age-limit">- 12 ans</div>
+
+                <div className="info tags">
+                  <strong>Mots clés</strong> : prophecy, based on novel or book, emperor, future, dystopia, sand, spice, army{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="movie-title">
+            <h3>Dune</h3>
+            <div className="year">2021</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="image-wrapper">
+            <div className="image">
+              <img src={dune} alt="Affiche de Dune" />
+            </div>
+            <div className="image-overlay">
+              <div className="content">
+                <div className="info synopsis">
+                  <strong>Synopsis</strong> : L'histoire de Paul Atreides, jeune homme aussi doué que brillant, voué à connaître un destin hors du
+                  commun qui le dépasse totalement. Car, s'il veut préserver l'avenir de sa famille et de son peuple, il devra se rendre sur Dune, la
+                  planète la plus dangereuse de l'Univers. Mais aussi la seule à même de fournir la ressource la plus précieuse capable de décupler la
+                  puissance de l'Humanité. Tandis que des forces maléfiques se disputent le contrôle de cette planète, seuls ceux qui parviennent à
+                  dominer leur peur pourront survivre…
+                </div>
+
+                <div className="info director">
+                  <strong>Réalisateur·ice</strong> : Denis Villeneuve
+                </div>
+
+                <div className="info language">
+                  <strong>Langue originale</strong> : Anglais
+                </div>
+
+                <div className="info age-limit">- 12 ans</div>
+
+                <div className="info tags">
+                  <strong>Mots clés</strong> : prophecy, based on novel or book, emperor, future, dystopia, sand, spice, army{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="movie-title">
+            <h3>Dune</h3>
+            <div className="year">2021</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="image-wrapper">
+            <div className="image">
+              <img src={dune} alt="Affiche de Dune" />
+            </div>
+            <div className="image-overlay">
+              <div className="content">
+                <div className="info synopsis">
+                  <strong>Synopsis</strong> : L'histoire de Paul Atreides, jeune homme aussi doué que brillant, voué à connaître un destin hors du
+                  commun qui le dépasse totalement. Car, s'il veut préserver l'avenir de sa famille et de son peuple, il devra se rendre sur Dune, la
+                  planète la plus dangereuse de l'Univers. Mais aussi la seule à même de fournir la ressource la plus précieuse capable de décupler la
+                  puissance de l'Humanité. Tandis que des forces maléfiques se disputent le contrôle de cette planète, seuls ceux qui parviennent à
+                  dominer leur peur pourront survivre…
+                </div>
+
+                <div className="info director">
+                  <strong>Réalisateur·ice</strong> : Denis Villeneuve
+                </div>
+
+                <div className="info language">
+                  <strong>Langue originale</strong> : Anglais
+                </div>
+
+                <div className="info age-limit">- 12 ans</div>
+
+                <div className="info tags">
+                  <strong>Mots clés</strong> : prophecy, based on novel or book, emperor, future, dystopia, sand, spice, army{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="movie-title">
+            <h3>Dune</h3>
+            <div className="year">2021</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="image-wrapper">
+            <div className="image">
+              <img src={dune} alt="Affiche de Dune" />
+            </div>
+            <div className="image-overlay">
+              <div className="content">
+                <div className="info synopsis">
+                  <strong>Synopsis</strong> : L'histoire de Paul Atreides, jeune homme aussi doué que brillant, voué à connaître un destin hors du
+                  commun qui le dépasse totalement. Car, s'il veut préserver l'avenir de sa famille et de son peuple, il devra se rendre sur Dune, la
+                  planète la plus dangereuse de l'Univers. Mais aussi la seule à même de fournir la ressource la plus précieuse capable de décupler la
+                  puissance de l'Humanité. Tandis que des forces maléfiques se disputent le contrôle de cette planète, seuls ceux qui parviennent à
+                  dominer leur peur pourront survivre…
+                </div>
+
+                <div className="info director">
+                  <strong>Réalisateur·ice</strong> : Denis Villeneuve
+                </div>
+
+                <div className="info language">
+                  <strong>Langue originale</strong> : Anglais
+                </div>
+
+                <div className="info age-limit">- 12 ans</div>
+
+                <div className="info tags">
+                  <strong>Mots clés</strong> : prophecy, based on novel or book, emperor, future, dystopia, sand, spice, army{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="movie-title">
+            <h3>Dune</h3>
+            <div className="year">2021</div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pagination-wrapper">
+        <div className="pagination-mobile"></div>
+        <div className="pagination-desktop"></div>
+      </section>
     </div>
   );
 }
